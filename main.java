aedf098a770372b0d3c5781c2f73d952068dfa73
Adya Sengupta <adyasengupta23@gmail.com>
2025-01-25 12:00:25 -0800

    pseudo in main

---
+++ main.java
@@ -0,0 +1,30 @@
+taskList = Map(taskID, arrayList(dueDate, difficulty, category, time))
+hoursAvailable = 12
+boolean [] available = new int[12];
+
+	// user blocks out time that is unavailable
+	setUp()
+		getUserInput(startTime, endTime, event)
+		hours = calculate(startTime, endTime)
+		hoursAvailable - hours
+		// mark hours in available array as false
+
+	//user add a new task
+taskAdd (dueDate, difficulty, category, time = est(category))
+		new list (dueDate, difficulty, category, time)		
+		
+map[taskID, newList]
+
+scheduler();
+
+//needs task, schedule, helper function for estimating time completion
+//scheduler() {
+
+
+//estimates time it will take a task to complete- needs to store history somehow? 
+timeEstimate(dueDate, time, Category)
+
+//method to process data once it is completed- data analysis
+
+//remindres
+
