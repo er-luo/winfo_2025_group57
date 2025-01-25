12ea9ad89c0ab4ce4882ddad70dc7c37c0336a57
riyaurs04 <riyaurs@uw.edu>
2025-01-25 13:58:02 -0800

    changed to pqueue

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
     static Map<Integer, ArrayList<Object>> taskList = new HashMap<>();
     static Map<String, Integer> weekHours = new HashMap<>();
     static int hoursAvailable = 24;
-	Queue<String> days = new LinkedList<>();
+	Queue<String> daysOfWeek = new PriorityQueue<String> ();
     static boolean grindset = false;
     static boolean[] available = new boolean[24]; // Represents 12 hours of availability
 
