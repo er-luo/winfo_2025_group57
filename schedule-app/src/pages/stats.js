9adf455e3d729d8d6ee1c398db27eae2fa1ac1c4
er-luo <er.caluo@gmail.com>
2025-01-25 18:19:52 -0800

    Update stats.js

--- schedule-app/src/pages/stats.js
+++ schedule-app/src/pages/stats.js
@@ -10,10 +10,10 @@ function Layout(props) {
         <div className="stats">
             <ul> <h3>Last week: </h3>
                 <li>You completed 6 out of 10 tasks!</li>
-                <li>You spent 5.4 hours on Exams</li>
-                <li>You spent 6.2 hours on Assignments</li>
-                <li>You spent 2.1 hours on Projects</li>
-                <li>You spent 3.8 hours on Quizzes</li>
+                <li>You spent 5.4 hours on Exams! That is an average of 3 hours per exam.</li>
+                <li>You spent 6.2 hours on Assignments! That is an average of 2.3 hours per assignment.</li>
+                <li>You spent 2.1 hours on Projects! That is an average of 2.1 hours per project.</li>
+                <li>You spent 3.8 hours on Quizzes! That is an average of 1.6 per quiz.</li>
             </ul>
         </div>
       </div>
